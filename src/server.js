c381fbaa2b09bfd3584b4ce206c69c50782f770a
Ebuka <eyaebuka@gmail.com>
2023-05-14 21:58:19 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,40 +1,44 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
 import userRouter from "./endpionts/user.js";
 
-
-
 const app = express();
-app.use(express.json()) 
-app.use(cors(
-    {origin: "*"}
-))
-
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 /* routes */
-app.use("/users", userRouter);
+app.use("/consumers", userRouter);
 
 /* errorhandlers */
 
-
 const port = process.env.PORT || 5001;
 
-app.get("/", (req,res)=>{
-    console.log("am here")
-    res.send("hello")
-})
+app.get("/", (req, res) => {
+  console.log("am here");
+  res.send("hello");
+});
+
+const options = {
+  DB_NAME: process.env.MONGO_CONSUMER_DB,
+};
 
 
 
-mongoose.connect(process.env.MONGO_CONNECTION, ()=>{
-    console.log("connected to mongo")
-
-    app.listen(port, ()=>{
-        
-        console.log(`listening on port ${port}`)
-        console.table(listEndpoints(app));
-    }
-    )
-})
+try {
+    mongoose.connect(process.env.MONGO_CONNECTION, () => {
+        console.log("connected to mongo");
+      
+        try {
+          app.listen(port, options, () => {
+            console.log(`listening on port ${port}`);
+            console.table(listEndpoints(app));
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      });
+} catch (error) {
+    console.log("mongo connection failed", error);
+}
